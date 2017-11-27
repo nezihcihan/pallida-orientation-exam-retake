fb5cb0c77726a052255eb31f496f634690e7ffb5
nezih <nezihcihan@hotmail.com>
2017-11-27 16:55:36 +0100

    created test for /warehouse/query endpoint, but failed

--- retake.exam/src/test/java/com/greenfox/retake/exam/ApplicationTests.java
+++ retake.exam/src/test/java/com/greenfox/retake/exam/ApplicationTests.java
@@ -1,16 +1,57 @@
 package com.greenfox.retake.exam;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.nio.charset.Charset;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
+@EnableWebMvc
 public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+			MediaType.APPLICATION_JSON.getSubtype(),
+			Charset.forName("utf8"));
+
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setup() throws Exception {
+		this.mockMvc = webAppContextSetup(webApplicationContext).build();
 	}
 
+	@Test
+	public void testArraySum() throws Exception {
+		mockMvc.perform(post("/warehouse/query")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"price\": 50}")
+				.content("{\"type\": lower}"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(contentType))
+				.andExpect(jsonPath("$.result", is("ok")))
+				.andExpect(jsonPath("$.productList[0].name", is("Strecth Steamed Pencil Skirt")))
+				.andExpect(jsonPath("$.productList", hasSize(2)));
+	}
 }
