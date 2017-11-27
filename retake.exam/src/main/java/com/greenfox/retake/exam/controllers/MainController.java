45ffb750873b61bd8b22db9476bc8f10c358363c
nezih <nezihcihan@hotmail.com>
2017-11-27 15:00:36 +0100

    dropdown selection form added to html, it recognisez the name of the products and lists them

--- retake.exam/src/main/java/com/greenfox/retake/exam/controllers/MainController.java
+++ retake.exam/src/main/java/com/greenfox/retake/exam/controllers/MainController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -16,7 +18,10 @@ public class MainController {
 
     @GetMapping("/warehouse")
     public String list(Model model) {
+
         model.addAttribute("products", productRepository.findAll());
         return "index";
     }
+
+    
 }
--- retake.exam/src/main/java/com/greenfox/retake/exam/repository/ProductRepository.java
+++ retake.exam/src/main/java/com/greenfox/retake/exam/repository/ProductRepository.java
@@ -3,6 +3,9 @@ package com.greenfox.retake.exam.repository;
 import com.greenfox.retake.exam.models.Product;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepository extends CrudRepository<Product, Integer> {
+import java.util.List;
 
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+    List<Product> findByNameContains(String name);
+    List<Product> findBySizeContains(String size);
 }
